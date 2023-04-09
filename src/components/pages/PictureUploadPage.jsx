d8166f1093e982280b4a9c500ccf2834db94d488
ruka\haneol <developereor@gmail.com>
2023-04-09 10:46:02 +0900

    package.json/Uploadpage

--- src/components/pages/PictureUploadPage.jsx
+++ src/components/pages/PictureUploadPage.jsx
@@ -1,37 +1,47 @@
-import React from 'react';
-import {View, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Platform,Button,StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+//import { Button } from '@components/atoms/Button'
 import Image from '@components/atoms/Image';
-import  Button from '@components/atoms/Button'
 
-export default function PictureUploadPage() {
-    const title = "사진업로드";
-    const styles = StyleSheet.create({
-        container: {
-          flex: 1,
-          backgroundColor: '#fff',
-          alignItems: 'center',
-          justifyContent: 'center',
-        },
+export default function ImagePickerExample() {
+ // const title = 'picture upload';
+  const [image, setImage] = useState(null);
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
 
-        image: {
-            width: 300,
-            height: 300,
-            borderRadius: 20,
-        },
-      });
+    image1: {
+        width: 300,
+        height: 300,
+        borderRadius: 20,
+    },
+  });
 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
 
-    return (
-        <View style={styles.container}>
-            <Image 
-                style={styles.image}
-                src={require('@assets/images/사토미.png')}
-                type='Profile'
-            />
-            <Button 
-                title={title} 
-                type='Long'
-            />
-        </View>
-    );
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {image && <Image src={{ uri: image }} style={styles.image1} type='Profile' />}
+      <Button title="사진선택하기" onPress={pickImage} />
+    </View>
+  );
 }
