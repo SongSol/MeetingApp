176bfb7e5c59888b268c47a53902e1fd63275b44
Seong Kihyeok <seongkh94@gmail.com>
2023-04-17 12:06:55 +0900

    사인업 백엔드 연결

--- src/components/pages/SignInPage.jsx
+++ src/components/pages/SignInPage.jsx
@@ -1,37 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CustomText from '@components/atoms/CustomText';
 import TextInput from '@components/atoms/TextInput';
 import {View, StyleSheet} from 'react-native';
-import { Button } from 'react-native-paper';
+import Button from '@components/atoms/Button';
+import axios from 'axios';
 
 export default function SignInPage({navigation}) {
-    const email = "EMAIL";
-    const password = "PASSWORD";
 
-    const styles = StyleSheet.create({
-        container: {
-          flex: 1,
-          backgroundColor: '#fff',
-          alignItems: 'center',
-          justifyContent: 'center',
-        },
-        button: {
-          width: 200,
-          height: 50,
-          margin: 20,
-        }
-    });
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      button: {
+        width: 200,
+        height: 50,
+        margin: 20,
+      }
+  });
+  
+  const getSignIn = async () => {
+    try {
+      const response = await axios.get(
+        'https://4ay6bv8m9k.execute-api.ap-northeast-1.amazonaws.com/getSignIn'
+        // , {
+        //   params: {
+        //     id: email,
+        //     password: password
+        //   }
+        // }
+      )
+      console.log(response);
+    } catch(e){
+      console.log(e);
+    }
+  }
+    
   return (
     <View style={styles.container}>
-      <CustomText text={email}/>
-      <TextInput label="email" placeholder={"emailを入力してください。"}/>
-      <CustomText text={password}/>
-      <TextInput label="password" placeholder={"passwordを入力してください。"} />
-      <Button mode="text" onPress={() => navigation.navigate('JudgeLookPage')}>
-        ログイン
-      </Button>
-        
+      <CustomText text={"EMAIL"}/>
+      <TextInput onChangeText={email => setEmail(email)} label="email" placeholder={"emailを入力してください。" }/>
+      <CustomText text={"PASSWORD"}/>
+      <TextInput onChangeText={password => setPassword(password)} label="password" placeholder={"passwordを入力してください。"} />
+      <Button type="long" title="ログイン" onPress={() => getSignIn()} />
     </View>
   );
 }
