3f735d5f231a573f89495b2427bffc178f132a2f
“songhaneol32” <developereor@gmail.com>
2023-05-22 13:26:08 +0900

    사진업로드 업데이트 사진찍기부분

--- src/components/pages/Judgefail.jsx
+++ src/components/pages/Judgefail.jsx
@@ -1,20 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text style={styles.text}>
-                안타깝게도 심사에 탈락하셨습니다. 
-            </Text>
-            <Text style={styles.text}>
-                다시 도전해주세요!!
-            </Text>
-			
-        	<StatusBar style="auto" />
-		</View>
-	);
-}
+import Button from '@components/atoms/Button'
 
 const styles = StyleSheet.create({
 	container: {
@@ -24,6 +10,25 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 	},
   text: {
-    fontSize: '20px',
+    fontSize: '25px',
   }
 });
+
+export default function InitialPage({navigation}) {
+	const title = '사진평가재도전';
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>
+                안타깝게도 심사에 탈락하셨습니다. 
+            </Text>
+            <Text style={styles.text}>
+                다시 도전해주세요!!
+            </Text>
+			<View style={{margin:30}}>
+			<Button title={title} type='Long' onPress={() => navigation.navigate('PictureUploadPage')} />
+			</View>
+        	<StatusBar style="auto" />
+		</View>
+	)
+};
+
--- src/components/pages/PictureUploadPage.jsx
+++ src/components/pages/PictureUploadPage.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Platform,StyleSheet,TouchableHighlight,Alert,Image } from 'react-native';
+import React, { memo, useState, useEffect,useCallback } from 'react';
+import { ActionSheetIOS, View, Text,Modal,Platform,Pressable,StyleSheet,TouchableHighlight,Alert,Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+//import {} from '@expo/react-native-action-sheet';
 import Button from '@components/atoms/Button'
 import {
   SafeAreaView,
@@ -12,17 +13,24 @@ import awsconfig from '@components/aws-exports';
 Amplify.configure(awsconfig);
 import axios from "axios";
 
-
 export default function ImagePickerExample() {
   const title = 'picture upload';
-  const icons = 'camera';
+  const icon = 'camera';
   const [filePath, setFilePath] = useState({});
   const [filePath1, setFilePath1] = useState({});
   const [filePath2, setFilePath2] = useState({});
+  const [filePath3, setFilePath3] = useState({});
   const [image, setImage] = useState("");
   const [image1, setImage1] = useState("");
   const [image2, setImage2] = useState("");
   const image_width='50%';
+  const imagePickerOption = {
+    mediaType: "photo",
+    maxWidth: 768,
+    maxHeight: 768,
+    includeBase64: Platform.OS === "android",
+  };
+
   const styles = StyleSheet.create({
     // container: {
     //   flex: 1,
@@ -45,9 +53,46 @@ export default function ImagePickerExample() {
       //marginBottom:10,
   },
   });
+  const styles1 = StyleSheet.create({
+    background: {
+      backgroundColor: "rgba(0,0,0,0,6)",
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    whiteBox: {
+      width: 300,
+      backgroundColor: "white",
+      borderRadius: 4,
+      elevation: 2,
+    },
+    actionButton: {
+      padding: 16,
+      flexDirection: "row",
+      alignItems: "center",
+    },
+    icon: {
+      marginRight: 8,
+    },
+    text: {
+      fontSize: 26,
+    },
+  });
+
+  const buttonstyle = StyleSheet.create({
+    button: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 12,
+      paddingHorizontal: 32,
+      borderRadius: 4,
+      elevation: 3,
+      backgroundColor: '#8CDBEB',
+    },
+  });
 
 
-  const pickImage = async () => {
+  const pickImage = useCallback(async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
@@ -62,7 +107,7 @@ export default function ImagePickerExample() {
       setFilePath(result);
       
     }
-  };
+  });
   const pickImage1 = async () => {
     // No permissions request is necessary for launching the image library
     let result1 = await ImagePicker.launchImageLibraryAsync({
@@ -95,6 +140,64 @@ export default function ImagePickerExample() {
       
     }
   };
+
+  
+  const pickImageCamera = useCallback(async () => {
+    const camera = await ImagePicker.requestCameraPermissionsAsync();
+    if (camera.status !== 'granted') {
+      Alert.alert('memoirアプリのカメラのアクセス許可をONにしてください');
+    }
+
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage(result.assets[0].uri);
+      setFilePath(result);
+    }
+  });
+
+  const pickImageCamera1 = useCallback(async () => {
+    const camera = await ImagePicker.requestCameraPermissionsAsync();
+    if (camera.status !== 'granted') {
+      Alert.alert('memoirアプリのカメラのアクセス許可をONにしてください');
+    }
+
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage1(result.assets[0].uri);
+      setFilePath1(result);
+    }
+  });
+
+  const pickImageCamera2 = useCallback(async () => {
+    const camera = await ImagePicker.requestCameraPermissionsAsync();
+    if (camera.status !== 'granted') {
+      Alert.alert('memoirアプリのカメラのアクセス許可をONにしてください');
+    }
+
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage2(result.assets[0].uri);
+      setFilePath2(result);
+    }
+  });
+
+
+  
 
 
   const uploadFile = async() => {
@@ -136,26 +239,93 @@ export default function ImagePickerExample() {
       updataPictureData();
 
   };
-  
+
+    
+    // 선택 모달 오픈
+    const modalOpen = () => {
+      if (Platform.OS === "android") { // 안드로이드
+        setModalVisible(true);  
+        visible = true;
+      } else { // iOS
+        ActionSheetIOS.showActionSheetWithOptions(
+          {
+            options: ["카메라로 촬영하기", "사진 선택하기", "취소"],
+            cancelButtonIndex: 2,
+          },
+          (buttonIndex) => {
+            if (buttonIndex === 0) {
+              pickImageCamera();
+            } else if (buttonIndex === 1) {
+              pickImage();
+            }
+          },
+        )
+      }
+    }
+
+        // 선택 모달 오픈
+        const modalOpen1 = () => {
+          if (Platform.OS === "android") { // 안드로이드
+            setModalVisible(true);  
+            visible = true;
+          } else { // iOS
+            ActionSheetIOS.showActionSheetWithOptions(
+              {
+                options: ["카메라로 촬영하기", "사진 선택하기", "취소"],
+                cancelButtonIndex: 2,
+              },
+              (buttonIndex) => {
+                if (buttonIndex === 0) {
+                  pickImageCamera1();
+                } else if (buttonIndex === 1) {
+                  pickImage1();
+                }
+              },
+            )
+          }
+        }
+
+            // 선택 모달 오픈
+    const modalOpen2 = () => {
+      if (Platform.OS === "android") { // 안드로이드
+        setModalVisible(true);  
+        visible = true;
+      } else { // iOS
+        ActionSheetIOS.showActionSheetWithOptions(
+          {
+            options: ["카메라로 촬영하기", "사진 선택하기", "취소"],
+            cancelButtonIndex: 2,
+          },
+          (buttonIndex) => {
+            if (buttonIndex === 0) {
+              pickImageCamera2();
+            } else if (buttonIndex === 1) {
+              pickImage2();
+            }
+          },
+        )
+      }
+    }
+
 
   return (
     <View style={{alignContent:'center',alignItems:'center',margin:10}}>
       <View style={{flexDirection: 'row',marginTop:"40%"}} >
         <View> 
-          <TouchableHighlight onPress={pickImage} >
+          <TouchableHighlight onPress={modalOpen} >
           <Image source={{ uri: image }} style={styles.image1}  />
           {/* <Image defaultSource={require('@assets/images/차은우.png')} source={{ uri: image }} style={styles.image1}  /> */}
           </TouchableHighlight>
         </View>
         <View style={{marginLeft:10}}>
           <View>  
-            <TouchableHighlight onPress={pickImage1} >
+            <TouchableHighlight onPress={modalOpen1} >
             <Image source={{ uri: image1 }} style={styles.image2}  />
             {/* <Image defaultSource={require('@assets/images/차은우.png')} source={{ uri: image }} style={styles.image1}  /> */}
             </TouchableHighlight>
           </View>
           <View style={{marginTop:10}}> 
-            <TouchableHighlight onPress={pickImage2} >
+            <TouchableHighlight onPress={modalOpen2} >
             <Image source={{ uri: image2 }} style={styles.image2}  />
             {/* <Image defaultSource={require('@assets/images/차은우.png')} source={{ uri: image }} style={styles.image1}  /> */}
             </TouchableHighlight>
@@ -163,7 +333,7 @@ export default function ImagePickerExample() {
         </View>
       </View>
       <View style={{margin:10}}>
-      <Button title={title} type='Long' onPress={uploadFile}  />
+      <Button icon={icon} title={title} type='Pink' onPress={uploadFile} style={{}} />
       </View>
     </View>
     
